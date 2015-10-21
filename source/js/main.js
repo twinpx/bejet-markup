45d4a49c7b79cfebab0a5a83935a52fa0d8c3c7a
Tatiana <djembuka@gmail.com>
2015-10-21 19:30:39 +0300

    Demo page with iframe

--- source/js/main.js
+++ source/js/main.js
@@ -10,3 +10,11 @@ $( window ).scroll( function() {
 }).scroll();
 
 $( 'form' ).form();
+
+var $demoIframe = $( '#demo-iframe' );
+var $window = $(window);
+if ( $demoIframe.length ) {
+  $window.resize( function() {
+    $demoIframe.height( $window.height() - $demoIframe.offset().top );
+  }).resize();
+}
